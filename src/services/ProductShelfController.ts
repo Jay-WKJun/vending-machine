f3276937f9c664f7e977266740f7d8917775117a
KyungJunWoo <natcho9010@gmail.com>
2025-05-04 15:12:01 +0900

    add: ProductShelfController

---
+++ src/services/ProductShelfController.ts
@@ -0,0 +1,25 @@
+import type { Product } from "../types/product";
+
+export class ProductShelfController {
+  private products: Product[] = [
+    { id: 1, name: "콜라", price: 1100 },
+    { id: 2, name: "물", price: 600 },
+    { id: 3, name: "커피", price: 700 },
+  ];
+
+  // 상품 정보 조회
+  getProductInfo(productId: number): Product | null {
+    const product = this.products.find((p) => p.id === productId);
+    return product ?? null;
+  }
+
+  // 상품 구매 가능 여부 확인
+  isAvailable(productId: number) {
+    return Boolean(this.getProductInfo(productId));
+  }
+
+  // 상품 구매 처리
+  purchase(productId: number): Product | null {
+    return this.getProductInfo(productId);
+  }
+}
